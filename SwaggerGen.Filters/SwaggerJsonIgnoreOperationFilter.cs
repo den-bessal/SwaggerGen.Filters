8cb6dfc9e5e1f2ed1759718196a205dc26bfedc6
Денис Бессалый <den_bessal@outlook.com>
2024-02-03 12:39:26 +0300

    Добавьте файлы проекта.

---
+++ SwaggerGen.Filters/SwaggerJsonIgnoreOperationFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace SwaggerGen.Filters
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.OpenApi.Models;
+    using Newtonsoft.Json;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+
+    public sealed class SwaggerJsonIgnoreOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var ignoredProperties = context.MethodInfo.GetParameters()
+                                                      .SelectMany(p => p.ParameterType.GetProperties()
+                                                                                      .Where(y => y.GetCustomAttribute<JsonIgnoreAttribute>() != null));
+
+            foreach (var property in ignoredProperties)
+                operation.Parameters = operation.Parameters
+                    .Where(p => !p.Name.Equals(property.Name, StringComparison.InvariantCulture)).ToList();
+        }
+    }
+}
---
+++ SwaggerGen.Filters/SwaggerQueryParameterRequiredFilter.cs
@@ -0,0 +1,42 @@
+﻿namespace SwaggerGen.Filters
+{
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.OpenApi.Models;
+    using Newtonsoft.Json;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+
+    public sealed class SwaggerQueryParameterRequiredFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (!context.ApiDescription.ParameterDescriptions.Any())
+            {
+                return;
+            }
+
+            var parameterDescriptions = context.ApiDescription.ParameterDescriptions;
+
+            foreach (var parameterDescription in parameterDescriptions)
+            {
+                JsonRequiredAttribute jsonRequiredAttribute = null;
+                var type = parameterDescription.ModelMetadata?.ContainerType;
+
+                if (type != null)
+                {
+                    jsonRequiredAttribute = type.GetProperty(parameterDescription.Name)?.GetCustomAttribute<JsonRequiredAttribute>();
+                }
+
+                if (jsonRequiredAttribute != null)
+                {
+                    var parameter = operation.Parameters.SingleOrDefault(p => p.Name == parameterDescription.Name);
+
+                    if (parameter != null)
+                    {
+                        parameter.Required = true;
+                    }
+                }
+            }
+        }
+    }
+}
